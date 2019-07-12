2c395091b78d237784747abb2d7eebaaf5bde229
Derek S <dschwan21@me.com>
2019-07-12 08:41:03 -0700

    added get requests for action router and project router

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -3,4 +3,19 @@ const actionDb = require("../helpers/actionModel");
 const projectDb = require("../helpers/projectModel");
 const router = express.Router();
 
+//Get request for actions with a specific ID
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const actions = await actionDb.get(id);
+    if (actions) {
+      res.status(200).json(actions);
+    } else {
+      res.status(400).json({ message: "Unable to locate actions" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error trying to get actions" });
+  }
+});
+
 module.exports = router;
--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -2,4 +2,55 @@ const express = require("express");
 const projectDb = require("../helpers/projectModel");
 const router = express.Router();
 
+//Get projects list and project by ID and actions of a project by its ID
+
+router.get("/", async (req, res) => {
+  try {
+    const projects = await projectDb.get();
+    if (projects) {
+      res.status(200).json(projects);
+    } else {
+      res.status(400).json({ message: "Unable to locate projects" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error trying to get projects" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const project = await projectDb.get(id);
+    console.log("the project is:", project);
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res
+        .status(400)
+        .json({ message: "Unable to locate project with that id" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error trying to get project" });
+  }
+});
+
+router.get("/:id/actions", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const findProj = await projectDb.get(id);
+    console.log("is the project found:", findProj);
+    const projectActions = await projectDb.getProjectActions(id);
+    if (findProj) {
+      res.status(200).json(projectActions);
+    } else {
+      res.status(400).json({
+        message: "Unable to locate actions from a project with that id"
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error trying to get actions" });
+  }
+});
+
 module.exports = router;
