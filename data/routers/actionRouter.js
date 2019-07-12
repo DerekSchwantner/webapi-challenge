8ed3f3637e9de571061241fb2e41f3adfbc0a58b
Derek S <dschwan21@me.com>
2019-07-12 08:15:27 -0700

    first commit, server up and running

---
+++ data/routers/actionRouter.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const actionDb = require("../helpers/actionModel");
+const projectDb = require("../helpers/projectModel");
+const router = express.Router();
+
+module.exports = router;
---
+++ data/routers/projectRouter.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const projectDb = require("../helpers/projectModel");
+const router = express.Router();
+
+module.exports = router;
